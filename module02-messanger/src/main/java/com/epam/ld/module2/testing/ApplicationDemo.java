a9f06681e1643aa34f7c4b920d2c89ea1e733eed
Konor8812 <konor881@gmail.com>
2023-05-30 17:14:39 +0300

    application starter tests created

--- module02-messanger/src/main/java/com/epam/ld/module2/testing/ApplicationDemo.java
+++ module02-messanger/src/main/java/com/epam/ld/module2/testing/ApplicationDemo.java
@@ -1,10 +1,12 @@
 package com.epam.ld.module2.testing;
 
+import java.util.HashMap;
+
 public class ApplicationDemo {
 
   private static ApplicationMode applicationMode;
   private static String[] args;
-
+  private static final HashMap<String, String> knownPlaceholders = new HashMap<>();
   public static void main(String[] args) {
     if (args.length == 0) {
       applicationMode = ApplicationMode.CONSOLE;
@@ -16,8 +18,26 @@ public class ApplicationDemo {
     ApplicationDemo.args = args;
   }
 
+  public static void start(){
+    switch (applicationMode){
+      case CONSOLE -> startInConsoleMode();
+      case FILE -> startInFileMode();
+      default -> throw new ApplicationException("Application mode not specified");
+    }
+  }
+
+  private static void startInConsoleMode() {
+
+  }
+  private static void startInFileMode() {
+
+  }
 
   public static ApplicationMode getApplicationMode() {
     return applicationMode;
+  }
+
+  public static HashMap<String, String> getKnownPlaceholders() {
+    return knownPlaceholders;
   }
 }
--- module02-messanger/src/test/java/com/epam/ld/module2/testing/ApplicationDemoTest.java
+++ module02-messanger/src/test/java/com/epam/ld/module2/testing/ApplicationDemoTest.java
@@ -2,12 +2,31 @@ package com.epam.ld.module2.testing;
 
 import static com.epam.ld.module2.testing.ApplicationMode.CONSOLE;
 import static com.epam.ld.module2.testing.ApplicationMode.FILE;
+import static com.epam.ld.module2.testing.constants.TestConstants.SENDER_NAME;
+import static com.epam.ld.module2.testing.constants.TestConstants.SENDER_PLACEHOLDER;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 public class ApplicationDemoTest {
+
+  @TempDir
+  Path tempDir;
+  String inputFilename = "fileWithPlaceholders.properties";
+  String outputFilename = "output.txt";
+
 
   @Test
   public void applicationShouldStartInConsoleModeIfNoArgsPassed() {
@@ -25,4 +44,70 @@ public class ApplicationDemoTest {
   public void applicationStartShouldThrowExceptionIfInvalidArgsNumber() {
     assertThrows(ApplicationException.class, () -> ApplicationDemo.main(new String[]{"", "", ""}));
   }
+
+  @Test
+  public void consoleModeShouldReadAndWriteToConsole() throws IOException {
+    var defaultIn = System.in;
+    var defaultOut = System.out;
+
+    var data = String.format("""
+        %s
+        %s
+        \\""", SENDER_PLACEHOLDER, SENDER_NAME).getBytes(StandardCharsets.UTF_8);
+
+    ApplicationDemo.main(new String[]{});
+    ApplicationDemo.start();
+
+    try (var inputStream = new ByteArrayInputStream(data);
+        var byteOs = new ByteArrayOutputStream();
+        var printStream = new PrintStream(byteOs)) {
+      // Imitation of console input and output
+      System.setIn(inputStream);
+      System.setOut(printStream);
+
+      // ensures data was written in System.out
+      System.out.print(new String(inputStream.readAllBytes()));
+      assertArrayEquals(data, byteOs.toByteArray());
+
+      // ensures data from imitated console input was received and parsed
+      assertEquals(1, ApplicationDemo.getKnownPlaceholders().size());
+      assertEquals(SENDER_NAME, ApplicationDemo.getKnownPlaceholders().get(SENDER_PLACEHOLDER));
+
+    } finally {
+      System.setIn(defaultIn);
+      System.setOut(defaultOut);
+    }
+  }
+
+  @Test
+  public void consoleModeShouldReadFromFileSystem() throws IOException {
+    var data = String.format("""
+        %s
+        %s
+        \\""", SENDER_PLACEHOLDER, SENDER_NAME).getBytes(StandardCharsets.UTF_8);
+
+    prepareFiles(data);
+
+    ApplicationDemo.main(new String[]{inputFilename, outputFilename});
+    ApplicationDemo.start();
+
+    assertEquals(1, ApplicationDemo.getKnownPlaceholders().size());
+    assertEquals(SENDER_NAME, ApplicationDemo.getKnownPlaceholders().get(SENDER_PLACEHOLDER));
+
+  }
+
+
+
+  private void prepareFiles(byte[] inputFileContent) throws IOException {
+    var file = tempDir.resolve(inputFilename);
+    Files.createFile(file);
+    Files.createFile(tempDir.resolve(outputFilename));
+    try (OutputStream outputStream = new FileOutputStream(file.toFile())) {
+      outputStream.write(inputFileContent);
+      outputStream.flush();
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
 }
