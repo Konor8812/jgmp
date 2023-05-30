f132501a79b9f212ec6b7492249556c82c6880fa
Konor8812 <konor881@gmail.com>
2023-05-30 12:09:45 +0300

    changes reversion

--- module02-messanger/src/main/java/com/epam/ld/module2/testing/template/exception/TemplateEngineException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.ld.module2.testing.template.exception;
-
-public class TemplateEngineException extends RuntimeException{
-
-  public TemplateEngineException(String msg) {
-    super(msg);
-  }
-}
