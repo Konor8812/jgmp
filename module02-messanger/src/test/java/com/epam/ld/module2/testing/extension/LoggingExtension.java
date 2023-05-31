3df1a24128d8ce3ae86a1524ae1a2c310014bb74
Konor8812 <konor881@gmail.com>
2023-05-31 17:38:59 +0300

    LoggingExtension now outputs logs to file

--- module02-messanger/src/test/java/com/epam/ld/module2/testing/extension/LoggingExtension.java
+++ module02-messanger/src/test/java/com/epam/ld/module2/testing/extension/LoggingExtension.java
@@ -1,30 +1,32 @@
 package com.epam.ld.module2.testing.extension;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 import org.junit.jupiter.api.extension.TestWatcher;
 
-public class LoggingExtension implements TestWatcher, TestExecutionExceptionHandler {
+public class LoggingExtension implements TestWatcher {
 
-  @Override
-  public void testSuccessful(ExtensionContext context) {
-    System.out.println("Test passed " + context.getDisplayName());
-  }
+  private static final String OUTPUT_FILE = "execution_info.txt";
 
   @Override
   public void testAborted(ExtensionContext context, Throwable cause) {
-    System.out.println("Test aborted " + context.getDisplayName());
+    try (PrintWriter writer = new PrintWriter(new FileWriter(OUTPUT_FILE, true))) {
+      writer.println("Test aborted " + context.getDisplayName());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 
   @Override
   public void testFailed(ExtensionContext context, Throwable cause) {
-    System.out.println("Test failed " + context.getDisplayName());
-  }
-
-  @Override
-  public void handleTestExecutionException(ExtensionContext context, Throwable throwable)
-      throws Throwable {
-    System.out.println("Test " + context.getDisplayName() + " execution caused exception "
-        + throwable.getMessage());
+    System.out.println("hh");
+    try (PrintWriter writer = new PrintWriter(new FileWriter(OUTPUT_FILE, true))) {
+      writer.println("Test failed " + context.getDisplayName());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
