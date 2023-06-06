65a61ae0ecf6b288fb8343712b9823017505ec46
Konor8812 <konor881@gmail.com>
2023-06-06 14:00:11 +0300

    partial mock added

--- module02-messanger/src/main/java/com/epam/ld/module2/testing/MailServer.java
+++ module02-messanger/src/main/java/com/epam/ld/module2/testing/MailServer.java
@@ -5,12 +5,22 @@ package com.epam.ld.module2.testing;
  */
 public class MailServer {
 
-    /**
-     * Send notification.
-     *
-     * @param addresses  the addresses
-     * @param messageContent the message content
-     */
-    public void send(String addresses, String messageContent) {
-    }
+  /**
+   * Send notification.
+   *
+   * @param addresses      the addresses
+   * @param messageContent the message content
+   */
+  public void send(String addresses, String messageContent) {
+
+  }
+
+  public String sendFast(String addresses, String messageContent) {
+    return "sent fast";
+  }
+
+  public String sendInstant(String addresses, String messageContent) {
+    return "sent instant";
+  }
+
 }
--- module02-messanger/src/test/java/com/epam/ld/module2/testing/MailServerTest.java
+++ module02-messanger/src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -1,11 +1,17 @@
 package com.epam.ld.module2.testing;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.mockito.Answers.CALLS_REAL_METHODS;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
+import org.mockito.Mockito;
 
 public class MailServerTest {
 
@@ -17,6 +23,22 @@ public class MailServerTest {
         .map(msg -> dynamicTest("Test for " + msg, () -> {
           assertDoesNotThrow(() -> mailServer.send("address", msg));
         }));
+  }
+
+
+  @Test
+  public void testPartialMock() {
+    var mailServer = Mockito.mock(MailServer.class, CALLS_REAL_METHODS);
+
+    when(mailServer.sendFast(any(), any()))
+        .thenReturn("mocked");
+
+    String result1 = mailServer.sendFast("", "");
+
+    String result2 = mailServer.sendInstant("", "");
+
+    assertEquals("mocked", result1);
+    assertEquals("sent instant", result2); // real implementation
   }
 
 }
