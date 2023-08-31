63ea4c3a7fd0d3f1cb190d5dbeac3f0f4c374758
Konor8812 <konor881@gmail.com>
2023-08-31 17:36:50 +0300

    module 10 extramile done

--- module10-spring-boot/src/main/java/com/illia/extramile/CustomActuatorEndpoint.java
+++ module10-spring-boot/src/main/java/com/illia/extramile/CustomActuatorEndpoint.java
@@ -1,6 +1,7 @@
 package com.illia.extramile;
 
 import com.illia.task3.repository.UserRepository;
+import io.micrometer.core.annotation.Counted;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
@@ -14,6 +15,7 @@ public class CustomActuatorEndpoint {
   private final UserRepository userRepository;
 
   @ReadOperation
+  @Counted(value = "custom_method_invocation_counter", description = "custom counter")
   public Long getUsersAmount() {
     return userRepository.count();
   }
